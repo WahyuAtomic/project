2ff9666da27a82f105d0dc410956c7f57894f4a4
WahyuAtomic <mangoleho111@com>
2024-03-09 16:12:05 +0000

    menambahkan dan merubah

---
+++ puzzle.js
@@ -0,0 +1,32 @@
+var rows = 3;
+var columns = 3;
+
+var currTile;
+var otherTile; //blank tile
+
+var turns = 0;
+
+//var imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var imgOrder = ["4", "2", "8", "5", "1", "6", "7", "9", "3"];
+
+window.onload = function () {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      //<img id="0-0" src="1png">.
+
+      let tile = document.createElement("img");
+      tile.id = r.toString() + "-" + c.toString();
+      tile.src = imgOrder.shift() + ".png";
+
+      //DRAG FUNCTIONALITY
+      tile.addEventListener("dragstart", dragStart); //click an to drag
+      tile.addEventListener("dragover", dragOver); //moving image around while clicked
+      tile.addEventListener("dragenter", dragEnter); //dragging image onto another one
+      tile.addEventListener("dragleave", dragLeave); //dragged image leaving another image
+      tile.addEventListener("drop", dragDrop); //drag an image over another image, drop the image
+      tile.addEventListener("dragend", dragEnd); //after drag drop, swap the two tiles
+
+      document.getElementById("board").append(tile);
+    }
+  }
+};
