782e3341a7b6090b312bc1b8ef85916e6150d350
WahyuAtomic <mangoleho111@com>
2024-03-10 16:29:05 +0000

    menambah

--- puzzle.js
+++ puzzle.js
@@ -30,3 +30,51 @@ window.onload = function () {
     }
   }
 };
+
+function dragStart() {
+  currTile = this; //this refers to the image tile being dragged
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+function dragEnter(e) {
+  e.preventDefault();
+}
+function dragLeave() {
+  e.preventDefault();
+}
+function dragDrop(e) {
+  otherTile = this; //this refers to the img tile being dropped on
+}
+function dragEnd(e) {
+  if (!otherTile.src.includes("3.png")) {
+    return;
+  }
+
+  let currCoords = currTile.id.split("-"); //ex) "0-0" -> ["0", "0"]
+  let r = parseInt(currCoords[0]);
+  let c = parseInt(currCoords[1]);
+
+  let otherCoords = otherTile.id.split("-");
+  let r2 = parseInt(otherCoords[0]);
+  let c2 = parseInt(otherCoords[1]);
+
+  let moveLeft = r == r2 && c2 == c - 1;
+  let moveRight = r == r2 && c2 == c + 1;
+
+  let moveUp = c == c2 && r2 == r - 1;
+  let moveDown = c == c2 && r2 == r + 1;
+
+  let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+
+  if (isAdjacent) {
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
+
+    currTile.src = otherImg;
+    otherTile.src = currImg;
+
+    turns += 1;
+    document.getElementById("turns").innerText = turns;
+  }
+}
